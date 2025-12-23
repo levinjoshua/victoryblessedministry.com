9488d5ce5ef532b0c36d9bafbc0676ac7353e8ea
levinjoshua <111196447+levinjoshua@users.noreply.github.com>
2025-12-23 17:24:59 +0300

    Update footer-injector.js

--- footer/footer-injector.js
+++ footer/footer-injector.js
@@ -1,11 +1,12 @@
 async function loadFooter(){
     try{
-        const response = await fetch('/footer/footer.html')
+        const response = await fetch('footer/footer.html')
         const footerHTML = await response.text()
         document.body.insertAdjacentHTML('beforeend', footerHTML)
     }catch (error){
         console.error('Error loading footer:', error)
     }
 }
+
 
 loadFooter()
